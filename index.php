93eca4bcbc4d370c82650c0bb0b98bf1b069cd7e
vagrant <vagrant@localhost.localdomain>
2018-04-21 17:05:21 +0900

    add function getImages()

--- index.php
+++ index.php
@@ -23,6 +23,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $uploader->upload();
 }
 
+$images = $uploader->getImages();
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -34,6 +36,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     text-align: center;
     font-family: Arial, sans-serif;
   }
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    margin-bottom: 5px;
+  }
   </style>
 </head>
 <body>
@@ -43,6 +53,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <input type="file" name="image">
     <input type="submit" value="upload">
   </form>
+
+  <ul>
+    <?php foreach ($images as $image) : ?>
+      <li>
+        <a href="<?php echo h(basename(IMAGES_DIR)) . '/' . basename($image); ?>">
+          <img src="<?php echo h($image); ?>">
+        </a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
 
 </body>
 </html>
